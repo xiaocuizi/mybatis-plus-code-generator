2a84c41d97065107eb0b10a4bc9f2d91abd98a77
eleven_4u <515503474@qq.com>
2020-04-03 14:29:41 +0800

    CodeGenerator

---
+++ src/main/java/com/gemini/MyBatisPlusApplication.java
@@ -0,0 +1,16 @@
+package com.gemini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xiaocuzi
+ */
+@SpringBootApplication
+public class MyBatisPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBatisPlusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gemini/MyBatisPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gemini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBatisPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
